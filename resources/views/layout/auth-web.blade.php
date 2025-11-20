164c1eef6eea7816f17cbdbf7c4a4941b4736cb9
unknown <anupshk39@gmail.com>
2025-11-20 23:11:42 +0545

    NoFOLLOW AND no index

--- resources/views/layout/auth-web.blade.php
+++ resources/views/layout/auth-web.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8" />
     <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon/favicon.ico') }}">
     @include('partials.head-css')
--- resources/views/layout/web.blade.php
+++ resources/views/layout/web.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8" />
     <title>@yield('title') | Khata App</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
 
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon/favicon.ico') }}">
